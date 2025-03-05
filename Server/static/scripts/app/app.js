5c1e32c71fcfc5d72d1a5ec3acfdc73828939b7b
bugs-pgcoe <bhargav@fsid-iisc.in>
2025-03-05 16:30:20 +0530

    05/02/2025

--- Server/static/scripts/app/app.js
+++ Server/static/scripts/app/app.js
@@ -15,26 +15,45 @@ function get(item) {
         });
 }
 
-var count = 0;
-function create_layout(input, result=null, _key=null) {
-    if (input instanceof Array) {
-        console.log(input);
-        Object.entries(input).forEach(([key, value]) => {
-            if (result === null) {
-                result = document.createElement(key);
-            }
-            if (value instanceof Object) {
-                create_layout(value, result)
-            }
-            else if (value instanceof Array) {
-                for (let i = 0; i < input.length; i++) {
-                    create_layout(value, result)
+function create_layout(input, parent_component = null) {
+    let main_component = null;
+
+    for (let i = 0; i < input.length; i++) {
+        const [tag, attributes] = Object.entries(input[i])[0];
+        const current_component = document.createElement(tag);
+
+        if (!main_component) {
+            main_component = current_component;
+        }
+
+        Object.entries(attributes).forEach(([key, value]) => {
+            if (Array.isArray(value) && value.length > 0 && typeof value[0] === "object") {
+                value.forEach(child => {
+                    const child_component = create_layout([child], current_component);
+                    current_component.appendChild(child_component);
+                });
+            } else {
+                if (key === "innerHTML") {
+                       current_component.appendChild(document.createTextNode(value));
                 }
+                else {
+                    current_component.setAttribute(key, value);
+                }
+
             }
-        else {
-                result.setAttribute(key, value);
-            }
-        })
+        });
+
+        if (parent_component) {
+            parent_component.appendChild(current_component);
+        }
     }
 
+    return main_component;
 }
+
+function add_component_to_div(component, div_id) {
+    var div = document.getElementById(div_id);
+    div.appendChild(component);
+}
+
+
